a9008089cb25e8e67352fee77b2136e04377dc4f
desaivaishnavi25 <desaivaishnavi25@gmail.com>
2023-07-05 22:48:58 +0530

    Initial commit

---
+++ stackbase/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+class Question(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=10000)
+    content = RichTextField()
+    likes = models.ManyToManyField(User,related_name='question_post')
+    dislikes = models.ManyToManyField(User,related_name='question_post_dislike')
+    date_created = models.DateTimeField(default = timezone.now) 
+    
+    def __str__(self):
+        return f'{self.user.username} - Question'
+    
+    def get_absolute_url(self):
+        return reverse('stackbase:question-detail',kwargs={'pk':self.pk})
+    
+    def total_likes(self):
+        return self.likes.count()
+    def total_dislikes(self):
+        return self.dislikes.count()
+
+class Comment(models.Model):
+    question = models.ForeignKey(Question, related_name="comment", on_delete = models.CASCADE)
+    name = models.CharField(max_length=1000)
+    content = RichTextField()
+    date_created = models.DateTimeField(default = timezone.now)
+    
+    def __str__(self):
+        return f'%s - %s' % (self.question.title, self.question.user)
+    
+    def get_absolute_url(self):
+        return reverse('stackbase:question-detail',kwargs={'pk':self.pk})
+    
+    def save(self, *args,**kwargs):
+        super().save(*args,**kwargs)
+    
